cf0dda63eddb2ca8e30b9e18cb31f206dcb4fb07
Alejandro Ramírez Fernández <alex.rf.46@gmail.com>
2021-12-28 17:40:02 +0100

    creating classes to define the architecture

---
+++ app/src/main/java/com/alejandroramirez/pikapika/data/PokemonRepository.kt
@@ -0,0 +1,7 @@
+package com.alejandroramirez.pikapika.data
+
+import com.alejandroramirez.pikapika.domain.PokemonRepository
+
+
+class PokemonRepository : PokemonRepository {
+}
---
+++ app/src/main/java/com/alejandroramirez/pikapika/domain/PokemonRepository.kt
@@ -0,0 +1,5 @@
+package com.alejandroramirez.pikapika.domain
+
+
+interface PokemonRepository {
+}
