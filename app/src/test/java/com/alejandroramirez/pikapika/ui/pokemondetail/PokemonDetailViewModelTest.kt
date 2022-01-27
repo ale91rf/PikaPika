97d2d1badfbd8d990472f6104f89c55aa13914ad
Alejandro Ramírez Fernández <alex.rf.46@gmail.com>
2022-01-27 18:40:21 +0100

    implemented test when starting with id

--- app/src/test/java/com/alejandroramirez/pikapika/ui/pokemondetail/PokemonDetailViewModelTest.kt
+++ app/src/test/java/com/alejandroramirez/pikapika/ui/pokemondetail/PokemonDetailViewModelTest.kt
@@ -1,13 +1,17 @@
 package com.alejandroramirez.pikapika.ui.pokemondetail
 
 import androidx.lifecycle.SavedStateHandle
+import com.alejandroramirez.pikapika.domain.PokemonObjectMother
 import com.alejandroramirez.pikapika.domain.usecase.GetPokemonByIdUseCase
+import com.alejandroramirez.pikapika.ui.Arg
 import com.alejandroramirez.pikapika.ui.CoroutineTestRule
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
 
 
 class PokemonDetailViewModelTest {
@@ -25,6 +29,26 @@ class PokemonDetailViewModelTest {
             error = PokemonDetailErrorType.INVALID_ID,
             isLoading = false
         )
+
+        pokemonDetailViewModel = PokemonDetailViewModel(
+            dispatcher = coroutinesTestRule.testDispatcherProvider,
+            stateHandle = stateHandle,
+            getPokemonByIdUseCase = getPokemonByIdUseCase
+        )
+
+        assertEquals(pokemonDetailViewModel.state.value, expectedResult)
+    }
+
+    @Test
+    fun `should set a pokemon with the state when starting by id and got that pokemon`() = runTest {
+        val pokemonId = 1
+        val pokemon = PokemonObjectMother.createPokemon(id = pokemonId)
+        val expectedResult = PokemonDetailViewState(
+            pokemon = pokemon,
+            isLoading = false
+        )
+        whenever(stateHandle.get<String>(Arg.POKEMON_ID_ARGUMENT)).thenReturn(pokemonId.toString())
+        whenever(getPokemonByIdUseCase(pokemonId.toString())).thenReturn(flowOf(pokemon))
 
         pokemonDetailViewModel = PokemonDetailViewModel(
             dispatcher = coroutinesTestRule.testDispatcherProvider,
